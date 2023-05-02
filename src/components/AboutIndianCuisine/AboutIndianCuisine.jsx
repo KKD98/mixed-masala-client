b94b14064c3845f93b77cd127897902e0fad0bc3
KKD98 <KajolKona98@gmail.com>
2023-05-02 22:42:53 +0600

    about indian cuisine section done

---
+++ src/components/AboutIndianCuisine/AboutIndianCuisine.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutIndianCuisine = () => {
+    return (
+        <div className='my-3'>
+        <h1 className='text-4xl text-center text-gray-900 font-sans font-bold mb-2'>About Indian Cuisine</h1>
+            <div className='bg-yellow-300'>
+            <p className='text-xl text-center text-gray-900 font-sans font-bold p-2 lg:p-5'>Indian cuisine is known for its diverse flavors, vibrant colors, and rich culinary heritage. It offers a wide range of vegetarian and non-vegetarian dishes that are prepared with a variety of herbs, spices, and seasonings, each adding a unique flavor to the dish. Popular Indian dishes include curries, biryanis, samosas, pakoras, and dal, among others. Indian cuisine is deeply rooted in family tradition and culture, and it plays a significant role in religious and social gatherings. With its unique flavors and rich cultural significance, Indian cuisine is a favorite among food enthusiasts around the world.</p>
+            </div>
+        </div>
+    );
+};
+
+export default AboutIndianCuisine;
--- src/components/HomeBody/HomeBody.jsx
+++ src/components/HomeBody/HomeBody.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import ChefsSection from '../ChefsSection/ChefsSection';
 import SignatureDishPart from '../SignatureDishPart/SignatureDishPart';
+import AboutIndianCuisine from '../AboutIndianCuisine/AboutIndianCuisine';
 
 const HomeBody = () => {
     return (
@@ -9,6 +10,7 @@ const HomeBody = () => {
             <Banner></Banner>
             <ChefsSection></ChefsSection>
             <SignatureDishPart></SignatureDishPart>
+            <AboutIndianCuisine></AboutIndianCuisine>
         </div>
     );
 };
