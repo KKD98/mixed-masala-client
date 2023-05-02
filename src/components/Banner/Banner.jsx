08d7a91dd023325d4cb925f45ee259547980b710
KKD98 <KajolKona98@gmail.com>
2023-05-02 20:03:30 +0600

    footer done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,8 +3,8 @@ import bannerImg from '../../assets/banner.jpg';
 
 const Banner = () => {
     return (
-        <div className='my-5 bg-yellow-100 flex items-center'>
-             <img className='w-[50%] mx-auto p-5' src={bannerImg} alt="" />
+        <div className='my-5 bg-yellow-100 flex flex-col lg:flex-row items-center'>
+             <img className='w-full lg:w-[50%] mx-auto p-5' src={bannerImg} alt="" />
              <div>
                 <h2 className='w-full text-gray-900 bg-yellow-300 text-center justify-center text-2xl font-semibold font-sans mx-auto p-8'>Unlock the rich flavors and diverse culinary <br /> traditions os India with our <br /> tantalizing quizzes and discover <br /> your inner foodie!</h2>
              </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { FaFacebook, FaInstagram , FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>
-            <h3>This is Footer</h3>
+        <div className='bg-yellow-300'>
+            <div className='flex gap-6 text-4xl text-gray-900 mx-auto py-5 justify-center'>
+                <FaFacebook></FaFacebook>
+                <FaInstagram></FaInstagram>
+                <FaTwitter></FaTwitter>
+            </div>
+            <div className='flex gap-3 text-xl justify-center text-gray-900 pb-3 font-semibold'>
+                <a className='hover:text-blue-800' href="">Terms of Use</a>
+                <a className='hover:text-blue-800' href="">Privacy Policy</a>
+            </div>
+            <p className='text-center text-md text-gray-600 pb-4'>© 2023 MixedMasala</p>
         </div>
     );
 };
