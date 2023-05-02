1e1b4124fbebfa535218b0f368c0ffc51fe4bb17
KKD98 <KajolKona98@gmail.com>
2023-05-02 17:08:35 +0600

    installed toastify , shows toast and button disable after click add to favorite button is done

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaRegStar, FaStar } from 'react-icons/fa';
 import Rating from 'react-rating';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Recipe = ({ recipe }) => {
-    const { recipeName, ingredients, cookingMethod, rating } = recipe;
+    const { recipeName, ingredients, cookingMethod, rating , id} = recipe;
+    const [disabled, setDisabled] = useState(false);
+    const handleToast = () => {
+
+                toast('🤩 Added to Favorites!', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    });
+                    setDisabled(true);
+    }
     return (
         <div>
+            <ToastContainer/>
             <div className="card w-full bg-yellow-100 shadow-xl mb-5 p-3">
                 <div className="card-body">
                     <h2 className="card-title text-2xl font-sans mx-auto mb-4  bg-yellow-400  p-3  font-bold text-gray-900 rounded">{recipeName}</h2>
@@ -20,7 +38,10 @@ const Recipe = ({ recipe }) => {
                                 fullSymbol={<FaStar></FaStar>}
                             />
                         </div>
-                        <button className="btn bg-yellow-400 text-gray-900 border-none hover:bg-yellow-600">Add to Favorite</button>
+
+
+                        <button disabled={disabled} onClick={() => {handleToast()}} className="btn bg-yellow-400 text-gray-900 border-none hover:bg-yellow-600">
+                            Add to Favorite</button>
                     </div>
                 </div>
             </div>
