fe524a1536acfabe2e1c9523437b71425ee30e26
KKD98 <KajolKona98@gmail.com>
2023-05-02 14:09:12 +0600

    after clicking view all recipe button ,  get that chef's data on ViewAllRecipe page

--- src/components/ViewAllRecipes/ViewAllRecipes.jsx
+++ src/components/ViewAllRecipes/ViewAllRecipes.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ViewAllRecipes = () => {
+    const [chef , setChef] = useState([]);
+    const [loading , setLoading] = useState(true);
+    const {id} = useParams();
+
+    useEffect(() => {
+        fetch('http://localhost:5000/allchefsdata')
+        .then(res => res.json())
+        .then(data => 
+            {
+            const findChef = data.find(element => element.id === parseInt(id));
+           setChef(findChef);
+           setLoading(false)
+            } 
+        )
+        .catch(error => console.log(error))
+    } , [])
+
+    console.log(chef)
+   
     return (
         <div>
-            <h3>This is ViewAllRecipes section</h3>
+            <h3>This is ViewAllRecipes section of id : {id}</h3>
+           
         </div>
     );
 };
